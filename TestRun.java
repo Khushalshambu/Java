85e650f2f58b20f8eec3dece7888f9fe828f44a1
Khushalshambu <92602822+Khushalshambu@users.noreply.github.com>
2022-10-16 02:14:29 -0400

    CodingBat Java String - countHi
    This code consists of solution to CodingBat Java String - countHi

--- TestRun.java
+++ TestRun.java
@@ -5,15 +5,19 @@ import java.util.*;
 
 public class TestRun {
     public static void main(String[] args) {
-        String str = "hi";
-        String s1 = "";
-        for (int i = 0;i<str.length();i++){
-        char c = str.charAt(i);
-        s1 = s1 + c + c;
+        String str = "ashihihihi";
+        int j = 0;
+        for(int i = 0;i<str.length();i++) {
+            if(str.length()>=2){
+            if (str.charAt(i) == 'h' && str.charAt(i + 1) == 'i') {
+                j += 1;
+            } }
+            else {
+                System.out.println("please give string which consists of 2 chars");
         }
-        System.out.println(s1);
+        System.out.println(j);
     }
-}
+} }
 
 
 
